1d277573588efd8c12d4a86bb04c7feeac3a4cd0
Leilah Joy Baruc <leilahjoy.baruc@cit.edu>
2025-09-24 00:46:12 +0800

    Backend Read

--- backend/app/src/main/java/com/notes/app/controller/NoteController.java
+++ backend/app/src/main/java/com/notes/app/controller/NoteController.java
@@ -1,6 +1,11 @@
 package com.notes.app.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +29,21 @@ public class NoteController {
     @PostMapping
     public Note create(@RequestBody Note note) {
         return noteService.createNote(note);
+    }
+      // READ ALL
+    @GetMapping
+    public ResponseEntity<List<Note>> getAllNotes() {
+        List<Note> notes = noteService.getAllNotes();
+        return ResponseEntity.ok(notes);
+    }
+
+    // READ ONE
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
+        Note note = noteService.getNoteById(id);
+        if (note == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(note);
     }
 }
